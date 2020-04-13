18248d5b1fba11872874a29455cac1e97df56c48
Cloud <hongzezhi_email@126.com>
2020-04-13 08:15:10 +0800

    zz

---
+++ src/main/java/com/example/annotation/enums/UniqueTypeEnum.java
@@ -0,0 +1,15 @@
+package com.example.annotation.enums;
+
+/**
+ * 唯一主键的类型
+ * */
+public enum UniqueTypeEnum {
+
+    SNOW("雪花算法"), UUID("uuid"),INCREMENT("自增");
+
+    private String name;
+
+    UniqueTypeEnum(String name){
+        this.name = name;
+    }
+}
